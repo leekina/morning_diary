d5d7b46382f48508ee2e340006c91894f3ce1c74
leekina <leemh332@gmail.com>
2023-06-03 23:29:34 +0900

    DESIGN : 폰트 추가

--- lib/screens/result_page.dart
+++ lib/screens/result_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:morning_diary/screens/start_page.dart';
 
 class ResultPage extends StatefulWidget {
   const ResultPage({super.key});
@@ -17,7 +16,13 @@ class _ResultPageState extends State<ResultPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('data'),
+            Text(
+              '15일 째 아침일기를 적고있습니다.\n 오늘의 부산은 비가올 것 같습니다. 우산을 챙겨가시길 바랍니다.',
+              style: TextStyle(
+                  fontFamily: 'Nanum',
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700),
+            ),
             Icon(
               Icons.image,
               size: MediaQuery.of(context).size.width,
