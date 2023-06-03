68dd3ec43bb84f2b9f6e389a09a3de3cd28869a7
leekina <leemh332@gmail.com>
2023-06-03 23:30:16 +0900

    REFACTOR : 컬러 리펙토링

---
+++ lib/constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color kQuestionBackgroundColor1 = Color(0xff8188d3);
+Color kQuestionBackgroundColor2 = Color(0xff8f95d8);
+List<Color> kCardColors = [
+  Color(0xffd7cee4),
+  Color(0xffebe3ec),
+  Color(0xffeee1d5),
+];
+
+List<Color> kCardColors2 = [
+  Color(0xff6C5B7B),
+  Color(0xffC06C84),
+  Color(0xffF67280),
+];
