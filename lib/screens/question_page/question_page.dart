c189203bc188864dd01e86edb994284e3049438a
leekina <leemh332@gmail.com>
2023-06-13 01:29:54 +0900

    FEAT : 디자인 임시 수정 및 sql 저장기능 추가

---
+++ lib/screens/question_page/question_page.dart
@@ -0,0 +1,110 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:morning_diary/components/size_config.dart';
+import 'package:morning_diary/models/question.dart';
+import 'package:morning_diary/models/sample.dart';
+import 'package:morning_diary/repository/sql_database_crud.dart';
+import 'package:morning_diary/screens/result_page.dart';
+
+class QuestionPage extends StatelessWidget {
+  static final Question _question = Question();
+
+  const QuestionPage({super.key});
+
+  void saveAnswer(List<String> answerList) async {
+    DateTime createAt = DateTime.now();
+    var answers = Sample(promiseAns: answerList, createAt: createAt);
+    await SqlSampleCrudRepository.create(answers);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> answerList = ['', '', ''];
+    return Scaffold(
+      backgroundColor: Color(0xFFF9E5DB),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Column(
+          children: [
+            VerticalSpacing(of: 50),
+            Text(_question.promiseQuestion[0]),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextField(
+                onChanged: (value) {
+                  answerList[0] = value;
+                },
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+            Text(_question.promiseQuestion[1]),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextField(
+                onChanged: (value) {
+                  answerList[1] = value;
+                },
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+            Text(_question.promiseQuestion[2]),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextField(
+                onChanged: (value) {
+                  answerList[2] = value;
+                },
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+            VerticalSpacing(of: 50),
+            ElevatedButton(
+              onPressed: () {
+                //데이터베이스에 저장
+                //saveAnswer(answerList);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ResultPage(),
+                    ));
+              },
+              style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  fixedSize: Size(150, 50),
+                  backgroundColor: Colors.white),
+              child: Text(
+                '저장',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
